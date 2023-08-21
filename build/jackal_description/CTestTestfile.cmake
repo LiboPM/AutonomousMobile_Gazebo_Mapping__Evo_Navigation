7321f82175bba4b24cbc7a2b7ae3bb8a7081f575
LiBo <98831970+LiboPM@users.noreply.github.com>
2023-08-21 17:46:39 +0800

    Delete build directory

--- build/jackal_description/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/libo/ME5413_Final_Project/src/jackal_description
-# Build directory: /home/libo/ME5413_Final_Project/build/jackal_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(_ctest_jackal_description_roslaunch-check_launch_description.launch "/home/libo/ME5413_Final_Project/build/catkin_generated/env_cached.sh" "/usr/bin/python3" "/opt/ros/noetic/share/catkin/cmake/test/run_tests.py" "/home/libo/ME5413_Final_Project/build/test_results/jackal_description/roslaunch-check_launch_description.launch.xml" "--return-code" "/usr/bin/cmake -E make_directory /home/libo/ME5413_Final_Project/build/test_results/jackal_description" "/opt/ros/noetic/share/roslaunch/cmake/../scripts/roslaunch-check -o \"/home/libo/ME5413_Final_Project/build/test_results/jackal_description/roslaunch-check_launch_description.launch.xml\" \"/home/libo/ME5413_Final_Project/src/jackal_description/launch/description.launch\" ")
-set_tests_properties(_ctest_jackal_description_roslaunch-check_launch_description.launch PROPERTIES  _BACKTRACE_TRIPLES "/opt/ros/noetic/share/catkin/cmake/test/tests.cmake;160;add_test;/opt/ros/noetic/share/roslaunch/cmake/roslaunch-extras.cmake;66;catkin_run_tests_target;/home/libo/ME5413_Final_Project/src/jackal_description/CMakeLists.txt;8;roslaunch_add_file_check;/home/libo/ME5413_Final_Project/src/jackal_description/CMakeLists.txt;0;")
--- build/master/velodyne_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/libo/ME5413_Final_Project/src/master/velodyne_description
-# Build directory: /home/libo/ME5413_Final_Project/build/master/velodyne_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
